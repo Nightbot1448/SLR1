0afc743c4cfb1b122e8008d605e4cd486832c444
Nightbot1448 <night1337bot@gmail.com>
2020-01-22 02:43:28 +0300

    init

---
+++ SLR(1).cpp
@@ -0,0 +1,12 @@
+﻿#include "SLR(1).h"
+#include <map>
+#include <utility>
+//#include <set>
+
+
+int main()
+{
+	//std::map<char, 
+	std::map<size_t, std::map<char, std::pair<char, size_t> > >
+	return 0;
+}
